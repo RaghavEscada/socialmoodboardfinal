d68e7ca5e55254825f2e71145949d1d2cdda7e67
Raghav Krishna <raghav@Raghavs-MacBook-Air.local>
2025-06-06 13:42:15 +0530

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -83,16 +83,7 @@ export default function Footer() {
                   <path fillRule="evenodd" d="M10 0a10 10 0 100 20 10 10 0 000-20zm4.613 4.614a1.49 1.49 0 01.431 1.05v8.672c0 .4-.156.784-.431 1.05a1.49 1.49 0 01-1.05.431H6.437c-.4 0-.784-.156-1.05-.431a1.49 1.49 0 01-.431-1.05V5.664c0-.4.156-.784.431-1.05a1.49 1.49 0 011.05-.431h7.126c.4 0 .784.156 1.05.431zM10 7.5a2.5 2.5 0 100 5 2.5 2.5 0 000-5zm3.5-1.5a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" clipRule="evenodd"/>
                 </svg>
               </a>
-              <a
-                href="https://wa.me/916385751370?text=Hi%20Raghav%2C%20I%20found%20your%20contact%20through%20the%20Nuke%20Marketing%20website.%20I%27m%20interested%20in%20discussing%20website%20development%20or%20AI%20automation%20services.%20Could%20we%20schedule%20a%20time%20to%20talk%3F"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="w-6 h-6 flex items-center justify-center bg-green-600 rounded hover:bg-green-700 transition-colors"
-              >
-                <svg className="w-4 h-4 text-white" fill="currentColor" viewBox="0 0 20 20">
-                  <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"/>
-                </svg>
-              </a>
+             
             </div>
           </div>
         </div>
