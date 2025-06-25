2a1dc413a3c6cc7a24b3a954c33fd56850515554
Raghav Krishna M <155087420+RaghavEscada@users.noreply.github.com>
2025-06-25 21:50:25 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -20,7 +20,7 @@ export default function Footer() {
                   />
                 </div>
                 <span className="text-4xl font-['Gilda_Display']">
-                  We do not do <span className="italic text-red-600">Ordinary.</span>
+                  We don't do <span className="italic text-red-600">Ordinary.</span>
                 </span>
               </div>
             </Link>
